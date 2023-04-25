b4999a8dbb2dc111103d5ffcaaca03534a7a4166
clovern <nclover7729@gmail.com>
2023-04-25 13:47:32 -0600

    improved popup windows

--- MealInfoDisplay.py
+++ MealInfoDisplay.py
@@ -3,6 +3,8 @@ from tkinter import messagebox
 from tkinter import ttk
 from PlanPage import PlanPage
 import webbrowser
+from PIL import Image
+from PIL import ImageTk
 from MealFileSaver import MealFileSaver
 from idlelib.tooltip import Hovertip
 
@@ -79,11 +81,23 @@ class MealInfoDisplay(PlanPage):
         information = meal_choice.format_meal_ingredients()
         
         if meal_choice.link != None:
-            information += "\n\nRecipe Link: \n\n"
-            self.details_popup= Toplevel()
-            self.details_popup.geometry("500x400")
+            information += "\n\nRecipe Link:\n"
+            self.details_popup= Toplevel(bg="white")
             self.details_popup.title("Details for " + meal_choice.name.title())
-            Label(self.details_popup, text= information).pack()
+            
+            bottom_frame = Frame(self.details_popup, bg="gray95")
+            bottom_frame.pack(side=BOTTOM, fill=X)
+            ttk.Button(bottom_frame, text="OK", width=12, default="active", command=self.details_popup.destroy).pack(anchor='se', pady = 10, padx = 15)
+            
+            left_frame = Frame(self.details_popup, width = 40, bg="white")
+            left_frame.pack(side=LEFT, fill=Y, padx=(15, 15))
+            self.info_icon = Image.open("./information_icon.png")
+            self.info_icon = (self.info_icon).resize((50,50))
+            self.info_icon = ImageTk.PhotoImage(self.info_icon)
+            self.info_icon_label = Label(left_frame, image=self.info_icon, bg="white", padx=10, pady=20)
+            self.info_icon_label.pack(anchor='n')
+
+            Label(self.details_popup, text= information, justify = LEFT, bg="white", wraplength=300).pack(anchor = "w", padx=(0, 20), pady=(15,0))
             self.create_hyperlink(meal_choice.link)
             return
         
@@ -99,10 +113,10 @@ class MealInfoDisplay(PlanPage):
 
     #Create a Label to display the link
     def create_hyperlink(self, link_text):
-        link = Label(self.details_popup, text=link_text, fg="blue", cursor="hand2")
+        link = Label(self.details_popup, text=link_text, fg="blue", cursor="hand2", bg="white", wraplength=300, justify=LEFT)
         link.bind("<Button-1>", lambda e:
         self.open_hyperlink(link_text))
-        link.pack()
+        link.pack(pady = (0, 15), padx=(0,20))
 
 
     def reroll_meal(self, mealday, meal, label):
